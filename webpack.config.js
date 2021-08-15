ea9f75eee36ca68946fdd64b0804b9922392bbc4
Fumiya Sato <fumist1028@gmail.com>
2021-08-15 20:38:34 +0900

    fix: devtool

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
 
   target: 'web',
 
-  devtool: NODE_ENV === 'production' ? 'none' : 'inline-source-map',
+  devtool: process.env.NODE_ENV === 'production' ? 'none' : 'inline-source-map',
 
   mode: 'none',
 };
